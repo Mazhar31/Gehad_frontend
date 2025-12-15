6fd3e1951dbb7bc61d76aea9dd972bf03fc25e5a
Mazhar31 <mzrbutt31@gmail.com>
2025-12-16 04:06:28 +0500

    merged the conflict

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -605,11 +605,11 @@ const LandingPage: React.FC<{ onNavigate: (page: 'login' | null) => void }> = ({
                                 </div>
                                 <div className="overflow-hidden rounded-2xl border border-border-color h-full min-h-[450px]">
                                     <iframe
-                                        src="https://maps.google.com/maps?q=dubai&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d462560.6828842949!2d54.897847!3d25.276987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f43496ad9c645%3A0xbde66e1147b4c5cd!2sDubai%20-%20United%20Arab%20Emirates!5e0!3m2!1sen!2s!4v1639000000000!5m2!1sen!2s"
                                         width="100%"
                                         height="100%"
-                                        style={{ border: 0, minHeight: '100%' }}
-                                        allowFullScreen={false}
+                                        style={{ border: 0, minHeight: '450px' }}
+                                        allowFullScreen={true}
                                         loading="lazy"
                                         referrerPolicy="no-referrer-when-downgrade"
                                         className="grayscale invert-[1] hue-rotate-[180deg]"
